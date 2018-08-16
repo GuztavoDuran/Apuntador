4e4d287179ae6187c182d048f8a729756ba7692c
Gustavo Blas Duran <gustavoblasduran1999@gmail.com>
2018-08-16 17:58:16 -0500

    ejercicio de apuntadores

---
+++ Apuntador.cpp
@@ -0,0 +1,18 @@
+#include iostream> 
+using namespace std;
+int main(){
+	int arr[]={0,1,2,3,4,5,6,7,8,9);
+	int *Apuntador;
+	int total=0, promedio=0;
+
+
+	Apuntador=$arr[0];
+	for(int i=0; i < 10; i++){
+		cout << "valor" << arr [i] << emdl;
+		total = total +*Apuntador++;
+	}
+	promedio = total / 10;
+	cout << "suma= " << total < endl;
+	cout << "promedio: " << promedio << endl;
+	return 0;
+}
