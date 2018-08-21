a086f2cf700fb1103938249b76c051ac43ff8cbf
GuztavoDuran <42410146+GuztavoDuran@users.noreply.github.com>
2018-08-20 19:53:39 -0500

    Update Apuntador.cpp

--- Apuntador.cpp
+++ Apuntador.cpp
@@ -1,4 +1,4 @@
-#include iostream> 
+#include <iostream> 
 using namespace std;
 int main(){
 	int arr[]={0,1,2,3,4,5,6,7,8,9);
@@ -6,13 +6,14 @@ int main(){
 	int total=0, promedio=0;
 
 
-	Apuntador=$arr[0];
+	Apuntador=&arr[0];
 	for(int i=0; i < 10; i++){
-		cout << "valor" << arr [i] << emdl;
+		cout << "valor directo: " << &arr[i] << endl;
+		cout << "valor" << arr [i] << endl;
 		total = total +*Apuntador++;
 	}
 	promedio = total / 10;
-	cout << "suma= " << total < endl;
+	cout << "suma= " << total << endl;
 	cout << "promedio: " << promedio << endl;
 	return 0;
 }
